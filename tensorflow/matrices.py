b695ca7d3c59cb0665bc3435794442909687f3d4
wangrl2016 <wangrl2016@gmail.com>
2023-02-04 20:47:15 +0800

    Matrices

---
+++ tensorflow/matrices.py
@@ -0,0 +1,16 @@
+"""
+https://www.mathsisfun.com/algebra/matrix-introduction.html
+https://www.mathsisfun.com/algebra/matrix-multiplying.html
+"""
+
+import numpy as np
+
+if __name__ == '__main__':
+    a1 = np.array([[1, 2, 3], [4, 5, 6]])
+    a2 = np.array([[7, 8], [9, 10], [11, 12]])
+
+    output = a1.dot(a2)
+    print(output)
+
+    transpose = a1.T
+    print(transpose)
