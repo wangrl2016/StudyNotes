5f8d832acd1b543dfa3d3c897fda4e7f2503cca1
wangrl2016 <wangrl2016@gmail.com>
2023-02-05 10:13:38 +0800

    Tensor operations

---
+++ tensorflow/tensor_operations.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+# relu激活函数
+def native_relu(x):
+    assert len(x.shape) == 2
+
+    x = x.copy()
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            x[i, j] = max(x[i, j], 0)
+    return x
+
+
+# 2D张量相加
+def naive_add(x, y):
+    assert len(x.shape) == 2
+    assert x.shape == y.shape
+    x = x.copy()
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            x[i, j] += y[i, j]
+    return x
+
+
+if __name__ == '__main__':
+
+    x = np.random.random((2, 3))
+    print(x)
+    y = np.random.random((3,))
+    print(y)
+
+    z = np.maximum(x, y)
+    print(z)
+    pass
